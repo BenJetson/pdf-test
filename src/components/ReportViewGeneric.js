da54e125d00e9ff220ff59667c4ba43f37b5d1ca
Ben Godfrey <BenJetson@users.noreply.github.com>
2022-04-10 18:33:23 -0400

    Fix box shadow on Paper.

--- src/components/ReportViewGeneric.js
+++ src/components/ReportViewGeneric.js
@@ -98,7 +98,7 @@ const Paper = styled.div`
   border-radius: 10px;
   margin: 2rem;
   background-color: #fff;
-  boxshadow: rgba(0, 0, 0, 0.2) 0px 4px 5px -2px,
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 4px 5px -2px,
     rgba(0, 0, 0, 0.14) 0px 7px 10px 1px, rgba(0, 0, 0, 0.12) 0px 2px 16px 1px;
 `;
 
